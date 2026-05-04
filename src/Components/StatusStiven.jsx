5294782fc5d33ac53c2c31ef5d4449386e673c3f
stivenvera <stivenvera300@gmail.com>
2026-05-04 13:31:35 -0500

    status

---
+++ src/Components/StatusStiven.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const StatusStiven = () => {
+  return (
+    <>
+    <div>StatusStiven</div>
+    <div className="puntos">
+    <button>100</button>
+     <button>400</button>
+      <button>500 🥵</button>
+       <button>700 🥲</button>
+       
+
+
+
+
+    </div>
+    
+    
+    
+    
+    
+    </>
+  )
+}
