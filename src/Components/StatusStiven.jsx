41b9f7a34aec59496afc8a3e0177d21f152be0e7
stivenvera <stivenvera300@gmail.com>
2026-05-06 13:17:09 -0500

    prev

--- src/Components/StatusStiven.jsx
+++ src/Components/StatusStiven.jsx
@@ -8,10 +8,10 @@ export const StatusStiven = () => {
     <div>StatusStiven</div>
     <div className="puntos">
       <h1>puntos:{puntos}</h1>
-    <button onClick={()=>{setpuntos((prev)=>{prev + 1})}}>100 🥵</button>
-     <button onClick={()=>{setpuntos((prev)=>{prev + 2})}}>400 🥲</button>
-      <button onClick={()=>{setpuntos((prev)=>{prev + 3})}}>500 🥵</button>
-       <button onClick={()=>{setpuntos((prev)=>{prev + 4})}}>700 🥲</button>
+    <button onClick={()=>{setpuntos(prev=>prev + 1)}}>100 🥵</button>
+     <button onClick={()=>{setpuntos(prev=>prev + 2)}}>400 🥲</button>
+      <button onClick={()=>{setpuntos(prev=>prev + 3)}}>500 🥵</button>
+       <button onClick={()=>{setpuntos(prev=>prev + 4)}}>700 🥲</button>
        
 
 
