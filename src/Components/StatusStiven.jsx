fa16223f745e9e1a4b494174feedbea5989c4d69
stivenvera <stivenvera300@gmail.com>
2026-05-06 12:49:04 -0500

    estatus stiven

--- src/Components/StatusStiven.jsx
+++ src/Components/StatusStiven.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const StatusStiven = () => {
+
+  const[puntos,setpuntos]= useState(0 ) 
   return (
     <>
     <div>StatusStiven</div>
     <div className="puntos">
-    <button>100</button>
+    <button onClick={()=>{setpuntos((prev)=>{prev + 2})}}>100 </button>
      <button>400</button>
       <button>500 🥵</button>
        <button>700 🥲</button>
