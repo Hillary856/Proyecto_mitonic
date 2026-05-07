3b1cc872295078b0326aa9aca4b96a9a3d3557ab
Hillary856 <hillary3247@gmail.com>
2026-05-07 13:14:54 -0500

    main

--- src/Components/StatusHillary.jsx
+++ src/Components/StatusHillary.jsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react'
 export default function StatusHillary() {
 
     const [gemas, Setgemas] = useState(0);
+    const [Character, setCharacter] = useState ([])
 
 const getCharacter = async() => {
 const res = await fetch("https://rickandmortyapi.com/api/character")
 const data = await res.json()
+setCharacter(data.results)
 
 console.log(data)
 }
@@ -33,6 +35,13 @@ getCharacter()
 
     </div>
     
+    <h1>Personajes Rick and Morty (Hillary)</h1>
+    {
+      Character.map((char, index) => (
+        <li key={index}>{char.name}</li>
+      ))
+    }
+
     </>
    
   )
