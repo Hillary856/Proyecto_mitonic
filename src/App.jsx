bc60f4b4230cd60948e7fe2e24e07e9cddfc309e
Hillary856 <hillary3247@gmail.com>
2026-05-27 21:08:34 -0500

    Arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import StatusHillary from "./Components/StatusHillary"
 import { StatusStiven } from "./Components/StatusStiven"
  */
 
-import SeccionStiven from "./Components/SeccionEscenarios/SeccionStiven"
+import SeccionStiven from "./Components/SeccionStiven/SeccionStiven"
 import { SeccionHeader } from "./Components/SeccionHeader/SeccionHeader"
 import "./Components/SeccionHeader/SeccionHeader.css"
 import { SeccionInfoComic } from "./Components/SeccionInfoComic/SeccionInfoComic"
 import "./Components/SeccionInfoComic/SeccionInfoComic.css"
+
 
 export const App = () => {
   return (
@@ -20,6 +21,7 @@ export const App = () => {
      <SeccionHeader/>
      <SeccionInfoComic/>
      <SeccionStiven/>
+     
     </div>
   )
 }
