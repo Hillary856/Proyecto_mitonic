721c7dcc15438f67c3f44c089b4c715519f18c35
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-06-02 16:38:12 -0500

    rt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./Pages/Home"
 import Comic from "./Pages/Comic"
 
