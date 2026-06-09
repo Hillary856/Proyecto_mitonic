0cd60ada60a9cb5c8531e3245a4e776f52424058
Hillary856 <hillary3247@gmail.com>
2026-06-09 17:00:43 -0500

    Lottie Hillary

---
+++ src/Components/SeccionStiven/LottieHillary.jsx
@@ -0,0 +1,25 @@
+import LottieReact from "lottie-react";
+import stivenAnimation from "./stivenanimacion.json";
+import { useRef } from "react";
+
+export const LottieStiven = () => {
+
+const lottieRef = useRef()
+const reproducir = () => {
+    lottieRef.current.play()
+}
+
+  return(
+   <>
+   <div>
+    
+   </div>
+   
+   
+   
+   </>
+  
+
+     
+  )
+}
