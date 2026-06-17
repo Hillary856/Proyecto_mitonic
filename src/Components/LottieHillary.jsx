ac9147773f65d6c744153d2348fa77b2baa3c09d
Hillary856 <hillary3247@gmail.com>
2026-06-17 13:28:35 -0500

    Organizacion de Fondo H

--- src/Components/LottieHillary.jsx
+++ src/Components/LottieHillary.jsx
@@ -1,5 +1,7 @@
 import lottieReact from "lottie-react"
 import osopolarH from "../assets/osopolarH.json";
+import pinguinoAndres from  "../assets/pinguinoAndres.json"
+import "./styleHillary.css"
 import { useRef } from "react";
 
 const Lottie = lottieReact.default;
@@ -14,15 +16,30 @@ export const LottieHillary = () => {
     return (
         <>
 
-            <div onClick={Reproducir}>
-                <Lottie
-                    lottieRef={LottieRef}
-                    animationData={osopolarH}
-                    loop={true}
-                    autoplay={false}
-                    style={{ width: 500, height: 500 }}
-                />
+
+
+            <div className="pantalla">
+
+                <img className= "position-absolute fondo" src="../public/SVG/Fondo.svg" alt="" />
+                <img className= "position-absolute nube1" src="../public/SVG/Nube1.svg" alt="" />
+                <img className= "position-absolute nube2" src="../public/SVG/Nube2.svg" alt="" />
+                <img className= "position-absolute hielo" src="../public/SVG/Hielo.svg" alt="" />
+                <img className= "position-absolute pez" src="../public/SVG/pez.svg" alt="" />
+
+
+                <div onClick={Reproducir}>
+                    <Lottie
+                        lottieRef={LottieRef}
+                        animationData={osopolarH}
+                        loop={true}
+                        autoplay={false}
+                        style={{ width: 500, height: 500 }}
+                        className="OsoH"
+                    />
+                </div>
+
             </div>
+
 
 
         </>
