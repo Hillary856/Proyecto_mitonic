976a519ed6267efa4e882217572589c7f0db6a53
lu16253-nh <restrpocarvajalluisaf100@gmail.com>
2026-06-30 17:12:38 -0500

    Pantalla fondo 1

--- src/Pages/EscenaPortada.jsx
+++ src/Pages/EscenaPortada.jsx
@@ -6,46 +6,46 @@ import { useRef } from "react";
 export const EscenaPortada = () => {
   return (
     <>
-     <div className="portada-container vh-100 vw-100 d-flex justify-content-center align-items-center position-relative">
+      <div className="portada-container vh-100 vw-100 d-flex justify-content-center align-items-center position-relative">
 
-      {/* Fondo gris (placeholder para imagen futura) */}
-      <div className="fondo-gris-imagen position-relative w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        {/* Fondo gris (placeholder para imagen futura) */}
+        <div className=" position-relative w-100 h-100 d-flex flex-column justify-content-center align-items-center">
 
-      
+          <img className="position-absolute " src="../public/fondo-1.png" alt="" />
 
-        {/* Sol */}
-        <div className="position-relative d-flex align-items-center">
-          <div className="fondo-sol-parpadeando"></div>
+          {/* Sol */}
+          <div className="position-relative d-flex align-items-center">
+            <div className="fondo-sol-parpadeando">
+              <img className="fondo1" src="../public/AssetsH/sol2.svg" alt="" />
+            </div>
 
-          {/* Instrucción clic */}
-         
+
+          </div>
+
+          {/* Navegación inferior */}
+          <div className="navegacion-inferior position-absolute bottom-0 start-50 translate-middle-x mb-2 text-center">
+
+
+
+            <h2 className="fs-3 mb-1">INDICACIONES</h2>
+            <h3 className="fs-4">ARTICULOS</h3>
+          </div>
         </div>
 
-        {/* Navegación inferior */}
-        <div className="navegacion-inferior position-absolute bottom-0 start-50 translate-middle-x mb-2 text-center">
+        {/* Botón sonido */}
+        <div className="btn-sonido position-absolute d-flex align-items-center">
 
-         
 
-          <h2 className="fs-3 mb-1">INDICACIONES</h2>
-          <h3 className="fs-4">ARTICULOS</h3>
+          <button className="btn btn-warning rounded-circle sonido-circle"></button>
+        </div>
+
+        {/* Botón logo */}
+        <div className="btn-logo position-absolute d-flex align-items-center">
+          <button className="btn btn-danger rounded-circle logo-circle"></button>
+
+
         </div>
       </div>
-
-      {/* Botón sonido */}
-      <div className="btn-sonido position-absolute d-flex align-items-center">
-       
-
-        <button className="btn btn-warning rounded-circle sonido-circle"></button>
-      </div>
-
-      {/* Botón logo */}
-      <div className="btn-logo position-absolute d-flex align-items-center">
-        <button className="btn btn-danger rounded-circle logo-circle"></button>
-
-      
-      </div>
-    </div>
-  );
     </>
   )
 }
