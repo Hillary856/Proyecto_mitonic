aeb80f347d31429e2a1738b93c5aa8db2fd7dc5b
Hillary856 <hillary3247@gmail.com>
2026-07-01 13:27:58 -0500

    Animacion 1 portada

--- src/Pages/EscenaPortada.jsx
+++ src/Pages/EscenaPortada.jsx
@@ -8,41 +8,40 @@ export const EscenaPortada = () => {
     <>
       <div className="portada-container vh-100 vw-100 d-flex justify-content-center align-items-center position-relative">
 
-        {/* Fondo gris (placeholder para imagen futura) */}
-        <div className=" position-relative w-100 h-100 d-flex flex-column justify-content-center align-items-center">
 
-          <img className="fondo-1 " src="../public/fondo-1.png" alt="" />
+        <div
+          className="position-relative d-flex flex-column justify-content-center align-items-center"
+          style={{ width: "900px" }}
+        >
+
+          <img className="fondopri" src="../public/fondopri.png" alt="" />
 
           {/* Sol */}
-          <div className="position-relative d-flex align-items-center">
-            <div className="fondo-sol-parpadeando">
-              <img className="fondo1" src="../public/AssetsH/sol2.svg" alt="" />
-            </div>
-
-
+          <div className="fondo-sol-parpadeando">
+            <img className="fondo1" src="../public/AssetsH/sol2.svg" alt="" />
           </div>
 
-          {/* Navegación inferior */}
-          <div className="navegacion-inferior position-absolute bottom-0 start-50 translate-middle-x mb-2 text-center">
-
-
-
-            <h2 className="fs-3 mb-1">INDICACIONES</h2>
-            <h3 className="fs-4">ARTICULOS</h3>
+          {/* Botón sonido */}
+          <div className="btn-sonido position-absolute">
+            <button className="btn btn-warning rounded-circle sonido-circle"></button>
           </div>
-        </div>
 
-        {/* Botón sonido */}
-        <div className="btn-sonido position-absolute d-flex align-items-center">
+          {/* Botón logo */}
+          <div className="btn-logo position-absolute">
+            <button className="btn btn-danger rounded-circle logo-circle"></button>
+          </div>
 
+          <div className="navegacion-inferior position-absolute bottom-0 start-50 translate-middle-x mb-5">
 
-          <button className="btn btn-warning rounded-circle sonido-circle"></button>
-        </div>
+            <button className="menu-btn mb-3">
+              INDICACIONES
+            </button>
 
-        {/* Botón logo */}
-        <div className="btn-logo position-absolute d-flex align-items-center">
-          <button className="btn btn-danger rounded-circle logo-circle"></button>
+            <button className="menu-btn">
+              ARTÍCULOS
+            </button>
 
+          </div>
 
         </div>
       </div>
