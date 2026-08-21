da0b53711858b3055e47816ffc006aae65c1ee88
Hillary856 <hillary3247@gmail.com>
2026-08-21 01:12:13 -0500

    Imagenes SCequipo

--- src/Components/SeccionEquipo.jsx
+++ src/Components/SeccionEquipo.jsx
@@ -1,12 +1,36 @@
-import { Link } from "react-router-dom";
 import "./styleHillary.css";
-
 
 export const SeccionEquipo = () => {
   return (
     <section className="seccion-equipo">
 
-     
+      <div className="equipo-integrantes">
+
+        {/* LUISA C */}
+        <div className="integrante">
+          <img
+            src="/public/Luisa C.png"
+            alt="Luisa C"
+          />
+        </div>
+
+        {/* HILLARY */}
+        <div className="integrante">
+          <img
+            src="/public/Hillary C.png"
+            alt="Hillary"
+          />
+        </div>
+
+        {/* LUISA R */}
+        <div className="integrante">
+          <img
+            src="/public/Luisa R.png"
+            alt="Luisa R"
+          />
+        </div>
+
+      </div>
 
     </section>
   );
