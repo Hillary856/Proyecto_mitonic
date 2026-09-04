8da9f617ead72d4a1ab3dddb9ab5a837125e8f15
Hillary856 <hillary3247@gmail.com>
2026-09-03 23:06:12 -0500

    Arreglos Escenas

--- src/Pages/Comicc.jsx/Escena1.jsx
+++ src/Pages/Comicc.jsx/Escena1.jsx
@@ -1,32 +1,39 @@
 import "./Escenas.css";
 
 const Escena1 = () => {
-
   return (
-
     <div className="escena-1">
 
       <div className="escena-1-contenido">
 
-    
+
         <img
           src="/Escenarios/FondoCueva.svg"
           alt="Cueva"
           className="escena-1-fondo"
         />
 
-        <img
-          src="/Escenarios/fuegoAnto.svg"
-          alt="Fuego"
-          className="escena-1-fuego"
-        />
+        <svg
+          className="escena-1-overlay"
+          viewBox="0 0 1024 598"
+          preserveAspectRatio="none"
+        >
+
+
+          <image
+            href="/Escenarios/fuegoAnto.svg"
+            x="302"
+            y="185"
+            width="35"
+            height="75"
+          />
+
+        </svg>
 
       </div>
 
     </div>
-
   );
-
 };
 
 export default Escena1;
--- src/Pages/Comicc.jsx/Escena2.jsx
+++ src/Pages/Comicc.jsx/Escena2.jsx
@@ -1,16 +1,15 @@
 import "./Escenas.css";
 
 const Escena2 = () => {
-
   return (
-
     <div className="escena-2">
 
       <div className="escena-2-contenido">
 
-        {/* =========================================
+        {/* =================================================
             FONDO DE ATENAS
-        ========================================= */}
+            Sistema de coordenadas: 1024 × 598
+        ================================================= */}
 
         <img
           src="/Escenarios/FondoAtenas.svg"
@@ -18,67 +17,83 @@ const Escena2 = () => {
           className="escena-2-fondo"
         />
 
+        {/* =================================================
+            ELEMENTOS DE LA ESCENA
+            Todos utilizan el mismo sistema de
+            coordenadas del fondo.
+        ================================================= */}
 
-        {/* =========================================
-            ÁRBOL 1
-        ========================================= */}
+        <svg
+          className="escena-2-overlay"
+          viewBox="0 0 1024 598"
+          preserveAspectRatio="none"
+        >
 
-        <img
-          src="/Escenarios/arbol1.svg"
-          alt=""
-          className="escena-2-arbol-1"
-        />
+          {/* =================================================
+              ÁRBOL 1
+          ================================================= */}
 
+          <image
+            href="/Escenarios/arbol1.svg"
+            x="939"
+            y="79"
+            width="120"
+            height="278"
+          />
 
-        {/* =========================================
-            ÁRBOL 2
-        ========================================= */}
+          {/* =================================================
+              ÁRBOL 2
+          ================================================= */}
 
-        <img
-          src="/Escenarios/arbol2.svg"
-          alt=""
-          className="escena-2-arbol-2"
-        />
+          <image
+            href="/Escenarios/arbol2.svg"
+            x="914"
+            y="105"
+            width="70"
+            height="250"
+          />
 
+          {/* =================================================
+              ARBUSTO 1
+          ================================================= */}
 
-        {/* =========================================
-            ARBUSTO 1
-        ========================================= */}
+          <image
+            href="/Escenarios/arbusto1.svg"
+            x="890"
+            y="300"
+            width="150"
+            height="100"
+          />
 
-        <img
-          src="/Escenarios/arbusto1.svg"
-          alt=""
-          className="escena-2-arbusto-1"
-        />
+          {/* =================================================
+              ARBUSTO 2
+          ================================================= */}
 
+          <image
+            href="/Escenarios/arbusto2.svg"
+            x="425"
+            y="323"
+            width="90"
+            height="85"
+          />
 
-        {/* =========================================
-            ARBUSTO 2
-        ========================================= */}
+          {/* =================================================
+              ARBUSTO 3
+          ================================================= */}
 
-        <img
-          src="/Escenarios/arbusto2.svg"
-          alt=""
-          className="escena-2-arbusto-2"
-        />
+          <image
+            href="/Escenarios/arbusto3.svg"
+            x="223"
+            y="300"
+            width="110"
+            height="105"
+          />
 
-
-        {/* =========================================
-            ARBUSTO 3
-        ========================================= */}
-
-        <img
-          src="/Escenarios/arbusto3.svg"
-          alt=""
-          className="escena-2-arbusto-3"
-        />
+        </svg>
 
       </div>
-
     </div>
-
   );
-
 };
 
 export default Escena2;
