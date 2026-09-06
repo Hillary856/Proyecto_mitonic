c283ca38a93a41af454387e1a5ec75d4c525eec3
Hillary856 <hillary3247@gmail.com>
2026-09-06 03:10:23 -0500

    Lottie escena 1

--- src/Pages/Comicc.jsx/Escena1.jsx
+++ src/Pages/Comicc.jsx/Escena1.jsx
@@ -1,11 +1,80 @@
+import { useState } from "react";
+import lottieReact from "lottie-react";
+
+import CicloAdrianBody from "../../assets/CicloAdrianBody.json";
+import AdrianHablando from "../../assets/AdrianHablando.json";
+
+import img0 from "../../assets/AdrianBody/img_0.png";
+import img1 from "../../assets/AdrianBody/img_1.png";
+import img2 from "../../assets/AdrianBody/img_2.png";
+import img3 from "../../assets/AdrianBody/img_3.png";
+import img4 from "../../assets/AdrianBody/img_4.png";
+import img5 from "../../assets/AdrianBody/img_5.png";
+import img6 from "../../assets/AdrianBody/img_6.png";
+import img7 from "../../assets/AdrianBody/img_7.png";
+
 import "./Escenas.css";
 
+const Lottie = lottieReact.default;
+
 const Escena1 = () => {
+
+  /* =====================================================
+     IMÁGENES DEL CICLO DE CAMINATA
+  ===================================================== */
+
+  const imagenesAdrian = [
+    img0,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7
+  ];
+
+
+  /* =====================================================
+     PREPARAR JSON DE ADRIÁN CAMINANDO
+  ===================================================== */
+
+  const animacionAdrian = {
+    ...CicloAdrianBody,
+
+    assets: CicloAdrianBody.assets.map((asset, index) => ({
+      ...asset,
+      u: "",
+      p: imagenesAdrian[index]
+    }))
+  };
+
+
+  /* =====================================================
+     ESTADO
+  ===================================================== */
+
+  const [adrianHabla, setAdrianHabla] = useState(false);
+
+
+  /* =====================================================
+     CUANDO TERMINA LA CAMINATA
+  ===================================================== */
+
+  const terminarCaminata = () => {
+    setAdrianHabla(true);
+  };
+
+
   return (
     <div className="escena-1">
 
       <div className="escena-1-contenido">
 
+
+        {/* =================================================
+            FONDO DE LA CUEVA
+        ================================================= */}
 
         <img
           src="/Escenarios/FondoCueva.svg"
@@ -13,12 +82,16 @@ const Escena1 = () => {
           className="escena-1-fondo"
         />
 
+
+        {/* =================================================
+            FUEGO
+        ================================================= */}
+
         <svg
           className="escena-1-overlay"
           viewBox="0 0 1024 598"
           preserveAspectRatio="none"
         >
-
 
           <image
             href="/Escenarios/fuegoAnto.svg"
@@ -29,6 +102,52 @@ const Escena1 = () => {
           />
 
         </svg>
+
+
+        {/* =================================================
+            ADRIÁN CAMINANDO
+        ================================================= */}
+
+        {/* =================================================
+    ADRIÁN CAMINANDO
+================================================= */}
+
+        {!adrianHabla && (
+
+          <div
+            className="escena-1-adrian-prueba"
+            onAnimationEnd={terminarCaminata}
+          >
+
+            <Lottie
+              animationData={animacionAdrian}
+              loop={true}
+              autoplay={true}
+            />
+
+          </div>
+
+        )}
+
+
+        {/* =================================================
+    ADRIÁN HABLANDO
+================================================= */}
+
+        {adrianHabla && (
+
+          <div className="escena-1-adrian-hablando">
+
+            <Lottie
+              animationData={AdrianHablando}
+              loop={true}
+              autoplay={true}
+            />
+
+          </div>
+
+        )}
+
 
       </div>
 
